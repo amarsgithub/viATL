913153f1a3ce2be9f031981857af0b36334e87ba
Anh Hoang <hoangducanh11031995@gmail.com>
2018-10-06 14:49:05 -0400

    change to startAcitivity in PhotoList and Photo_list.java

--- app/src/main/java/com/example/amars/viatl/Photo_List.java
+++ app/src/main/java/com/example/amars/viatl/Photo_List.java
@@ -2,16 +2,7 @@ package com.example.amars.viatl;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.app.Activity;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
+
 public class Photo_List extends AppCompatActivity {
 
     @Override
