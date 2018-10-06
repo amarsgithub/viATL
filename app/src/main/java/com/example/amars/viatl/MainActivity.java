ccedbafc345c8edaad7d558edd230d15576ff355
Amar Suchak <amarsgithub@outlook.com>
2018-10-06 15:52:38 -0400

    Amar's changes

--- app/src/main/java/com/example/amars/viatl/MainActivity.java
+++ app/src/main/java/com/example/amars/viatl/MainActivity.java
@@ -1,7 +1,17 @@
 package com.example.amars.viatl;
 
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
+
+
 }
