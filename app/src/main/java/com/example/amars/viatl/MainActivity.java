82140d00d6ad30d6164c8c174614a3c480180373
Amar Suchak <amarsgithub@outlook.com>
2018-10-06 17:35:16 -0400

    Latest revision, it compiles!

--- app/src/main/java/com/example/amars/viatl/MainActivity.java
+++ app/src/main/java/com/example/amars/viatl/MainActivity.java
@@ -50,54 +50,55 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//
-//        mButtonChooseImage = findViewById(R.id.button_choose_image);
-//        mButtonUpload = findViewById(R.id.button_upload);
-//        mTextViewShowUploads = findViewById(R.id.text_view_show_uploads);
-//        mEditTextFileName = findViewById(R.id.edit_text_file_name);
-//        mImageView = findViewById(R.id.image_view);
-//        mProgressBar = findViewById(R.id.progress_bar);
-//
-//        mButtonChooseImage.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                openFileChoose();
-//            }
-//        });
-//
-//        mButtonUpload.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//            }
-//        });
-//
-//        mTextViewShowUploads.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//            }
-//        });
-//
+
+        mButtonChooseImage = findViewById(R.id.button_choose_image);
+        mButtonUpload = findViewById(R.id.button_upload);
+        mTextViewShowUploads = findViewById(R.id.text_view_show_uploads);
+        mEditTextFileName = findViewById(R.id.edit_text_file_name);
+        mImageView = findViewById(R.id.image_view);
+        mProgressBar = findViewById(R.id.progress_bar);
+
+        mButtonChooseImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFileChoose();
+            }
+        });
+
+        mButtonUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mTextViewShowUploads.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
-//
-//    private void openFileChoose()
-//    {
-//        Intent intent = new Intent();
-//        intent.setType("image/*");
-//        intent.setAction(Intent.ACTION_GET_CONTENT);
-//        startActivityForResult(intent, PICK_IMAGE_REQUEST);
-//    }
-//
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
-//                && data != null && data.getData() != null)
-//            mImageUri = data.getData();
-//        }
-//
-//    }
+
+    private void openFileChoose()
+    {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null) {
+            mImageUri = data.getData();
+        }
+    }
+
+}
 
 //    public void PhotoList(View view) {
 ////        Intent intent = new Intent(this, Photo_List.class);
@@ -126,4 +127,4 @@ public class MainActivity extends AppCompatActivity {
 //            Toast.makeText(PostImage.this, "You haven't picked Image",Toast.LENGTH_LONG).show();
 //        }
 //    }
-}
+
